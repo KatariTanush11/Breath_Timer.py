d13b476e8d916de10a071990a6188c4a099ad871
Siddharth Vudikavalasa <sidvud98@gmail.com>
2020-01-15 16:57:05 +0530

    Shifted The counting numbers to centre

--- Breath_Timer.py
+++ Breath_Timer.py
@@ -5,9 +5,9 @@ m=0
 while 1==1:
 	t+=1
 	if t%8==0:
-		print(0)
+		print("                    ",0)
 	else:
-		print(8-(t%8) )
+		print("                    ",8-(t%8))
 	time.sleep(1)
 	if t%8==0:
 		if s==1:
